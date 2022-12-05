97b989e4fa39042b43edb13a6621abb7ed0b75f6
jayant-bits05 <113102471+jayant-bits05@users.noreply.github.com>
2022-12-05 12:38:25 +0530

    Update stat.py

--- stat.py
+++ stat.py
@@ -24,4 +24,13 @@ def median(x):
   else:
     median = x[n//2]
     return median
+pi = 3.14159
+def area(radius):
+    return pi*(radius**2)
+def circumference(radius):
+    return 2*pi*radius
+def sphereSurface(radius):
+    return 4.0*area(radius)
+def sphereVolume(radius):
+    return (4.0/3.0)*pi*(radius**3)
 
